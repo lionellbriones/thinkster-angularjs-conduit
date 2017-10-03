d6f225f8436bf718eec4270d261432ccb746bb53
DIR\lionell.c.briones <lionell.c.briones@accenture.com>
2017-10-03 14:37:39 +0800

    Initial commit

---
+++ src/js/services/user.service.js
@@ -0,0 +1,23 @@
+export default class User {
+    constructor(AppConstants, $http) {
+        'ngInject';
+
+        this._AppConstants = AppConstants;
+        this._$http = $http;
+
+        this.current = null;
+    }
+
+    attemptAuth(type, credentials) {
+        let route = (type === 'login') ? '/login' : '';
+        return this._$http({
+            url: this._AppConstants.api + '/users' + route,
+            method: 'POST',
+            data: credentials
+        }).then((res) => {
+            this.current = res.data.user;
+
+            return res;
+        });
+    }
+}
